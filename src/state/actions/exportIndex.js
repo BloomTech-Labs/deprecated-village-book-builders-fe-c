666c5a29b2a5ae897c11ca2bedae1c8191c96b94
ajg7 <ajgebara@gmail.com>
2021-01-14 13:42:33 -0600

    made a separate file for the teacher's actions and made an export file for it

---
+++ src/state/actions/exportIndex.js
@@ -0,0 +1,3 @@
+import * as teacher from './teacherActions';
+
+export { teacher };
---
+++ src/state/actions/teacherActions.js
@@ -0,0 +1,4 @@
+import { axiosWithAuth } from '../../utils';
+
+export const EDIT_TEACHER_PROFILE = 'EDIT_TEACHER_PROFILE';
+export const editTeacherProfile = () => dispatch => {};
