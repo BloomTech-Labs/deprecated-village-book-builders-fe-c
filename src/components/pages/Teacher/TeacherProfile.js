e1a660633e8f8c132285d8c1ac0adde1f76c530a
ajg7 <ajgebara@gmail.com>
2021-01-14 13:36:04 -0600

    made rough skeleton of the TeacherProfile

--- src/components/pages/Teacher/TeacherProfile.js
+++ src/components/pages/Teacher/TeacherProfile.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { ComponentTitle, Button } from '../../common/';
+import { Label } from '../../common/ProfileStyle';
 
 const TeacherProfile = props => {
-  return <div></div>;
+  /*Make a Teacher Role in State to dynamic change the component. This must get the id of the teacher*/
+  /*Labels should be replaced with data from the backend*/
+
+  const editProfileHandler = event => {};
+
+  return (
+    <div>
+      <ComponentTitle titleText={'Teacher'} />
+      <img src={''} alt="teacher" />
+      <Label>Name:</Label>
+      <Label>Education:</Label>
+      <Label>Location:</Label>
+      <Label>Subjects:</Label>
+      <Label>Fun Fact:</Label>
+      <Button
+        classType={'edit-headmaster-profile'}
+        buttonText={'Edit Profile'}
+        handleClick={''}
+      />
+    </div>
+  );
 };
 
 export default TeacherProfile;
