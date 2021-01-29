fb5105434dac2547fa4d260c2defc79044812e2c
Daniel Antonio <dernlsenpai@gmail.com>
2021-01-28 20:29:17 -0800

    Created form fields specified to Stakeholder's design.

--- src/components/pages/Headmaster/Mentees/AddNewMentee.js
+++ src/components/pages/Headmaster/Mentees/AddNewMentee.js
@@ -71,6 +71,45 @@ const langOptions = [
 
 const subjects = ['Math', 'English', 'History', 'Science', 'Geography'];
 
+const timeZones = [
+  { label: 'UTC-11', value: 'UTC-11' },
+  { label: 'UTC-10', value: 'UTC-10' },
+  { label: 'UTC-9', value: 'UTC-9' },
+  { label: 'UTC-9:30', value: 'UTC-9:30' },
+  { label: 'UTC-8', value: 'UTC-8' },
+  { label: 'UTC-7', value: 'UTC-7' },
+  { label: 'UTC-6', value: 'UTC-6' },
+  { label: 'UTC-5', value: 'UTC-5' },
+  { label: 'UTC-4', value: 'UTC-4' },
+  { label: 'UTC-3', value: 'UTC-3' },
+  { label: 'UTC-3:30', value: 'UTC-3:30' },
+  { label: 'UTC-2', value: 'UTC-2' },
+  { label: 'UTC-1', value: 'UTC-1' },
+  { label: 'UTC+0', value: 'UTC+0' },
+  { label: 'UTC+1', value: 'UTC+1' },
+  { label: 'UTC+2', value: 'UTC+2' },
+  { label: 'UTC+3', value: 'UTC+3' },
+  { label: 'UTC+3:30', value: 'UTC+3:30' },
+  { label: 'UTC+4', value: 'UTC+4' },
+  { label: 'UTC+4:30', value: 'UTC+4:30' },
+  { label: 'UTC+5', value: 'UTC+5' },
+  { label: 'UTC+5:30', value: 'UTC+5:30' },
+  { label: 'UTC+5:45', value: 'UTC+5:45' },
+  { label: 'UTC+6:30', value: 'UTC+6:30' },
+  { label: 'UTC+7', value: 'UTC+7' },
+  { label: 'UTC+8', value: 'UTC+8' },
+  { label: 'UTC+8:45', value: 'UTC+8:45' },
+  { label: 'UTC+9', value: 'UTC+9' },
+  { label: 'UTC+9:30', value: 'UTC+9:30' },
+  { label: 'UTC+10', value: 'UTC+10' },
+  { label: 'UTC+10:30', value: 'UTC+10:30' },
+  { label: 'UTC+11', value: 'UTC+11' },
+  { label: 'UTC+12', value: 'UTC+12' },
+  { label: 'UTC+12:45', value: 'UTC+12:45' },
+  { label: 'UTC+13', value: 'UTC+13' },
+  { label: 'UTC+14', value: 'UTC+14' },
+];
+
 export default function AddNewMentee(props) {
   const [visible, setVisible] = useState(false);
   const [langs, setLangs] = useState(langOptions);
@@ -132,6 +171,26 @@ export default function AddNewMentee(props) {
                 <Select mode="multiple" placeholder="Subjects">
                   {subjects.map(sub => {
                     return <Select.Option value={sub}>{sub}</Select.Option>;
+                  })}
+                </Select>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item name="homeCountry" label="Home Country">
+                <Input placeholder="Country" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item name="homeTimeZone" label="Time Zone">
+                <Select placeholder="TimeZone">
+                  {timeZones.map(zone => {
+                    return (
+                      <Select.Option value={zone.value}>
+                        {zone.label}
+                      </Select.Option>
+                    );
                   })}
                 </Select>
               </Form.Item>
