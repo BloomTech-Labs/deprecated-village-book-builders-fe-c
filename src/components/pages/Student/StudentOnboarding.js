2e2fc8a8c3214a78e52cbeb2023970c5a7a7fd68
danpatrickoneill <danpatrickoneill@gmail.com>
2021-02-02 14:38:51 -0500

    Added imports

---
+++ src/components/pages/Student/StudentOnboarding.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import { axiosWithAuth } from '../../../../utils/axiosWithAuth';
+import {
+  InfoCircleOutlined,
+  EditOutlined,
+  PlusCircleOutlined,
+} from '@ant-design/icons';
+import { Divider, Input, Modal, List, Avatar, Select } from 'antd';
+import Button from '../../common/Button';
+import { connect } from 'react-redux';
+import {
+  checkToken,
+  fetchMentees,
+  editMentee,
+} from '../../../../state/actions/index';
+import MenteeForm from '../Headmaster/Mentees/MenteeForm';
+import MenteeProfile from '../Headmaster/Mentees/MenteeProfile';
