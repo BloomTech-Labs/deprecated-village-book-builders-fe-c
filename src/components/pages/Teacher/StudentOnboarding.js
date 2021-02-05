c0b9c6b4cb7b1460a4099b15cfb64df42f0273b3
hussainabutt <68175777+hussainabutt@users.noreply.github.com>
2021-02-05 13:22:12 -0500

    Student onboarding component and api call

--- src/components/pages/Teacher/StudentOnboarding.js
+++ src/components/pages/Teacher/StudentOnboarding.js
@@ -1,7 +1,178 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ComponentTitles from '../../../components/common/ComponentTitles';
+import { Divider, Input, Modal, List, Avatar, Select } from 'antd';
+import { connect } from 'react-redux';
+import {
+  checkToken,
+  fetchMentees,
+  editMentee,
+} from '../../../state/actions/index';
+import MenteeProfile from '../../../components/pages/Headmaster/Mentees/MenteeProfile';
 
-const studentOnboarding = () => {
-  return <div></div>;
+const StudentOnboarding = props => {
+  let menteesSelection = [...props.mentees];
+  const [search, setSearch] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [currentMentee, setCurrentMentee] = useState({});
+  const [searchBy, setSearchBy] = useState(
+    'Search for mentees by birthdate in YYYY-MM-DD format...'
+  );
+  const searchHandler = e => setSearch(e.target.value);
+
+  const menteeStyles = {
+    newMentee: {
+      border: 'none',
+      backgroundColor: '#ff914d',
+      fontFamily: 'Caveat Brush,cursive',
+      fontStyle: 'italic',
+      webkitLetterSpacing: '2px',
+      mozLetterSpacing: '2px',
+      msLetterSpacing: '2px',
+      height: 'none',
+      letterSpacing: '2px',
+      fontWeight: '400',
+      fontSize: '22px',
+      color: 'white',
+      borderRadius: '18px',
+      padding: '8px',
+      paddingLeft: '15px',
+      paddingRight: '15px',
+      webkitAppearance: 'none',
+      margin: '1rem 0',
+      textAlign: 'right',
+      margin: '1rem 0',
+      width: 'auto',
+    },
+    moreInfo: {
+      border: 'none',
+      backgroundColor: '#ff914d',
+      fontFamily: 'Caveat Brush,cursive',
+      fontStyle: 'italic',
+      webkitLetterSpacing: '2px',
+      mozLetterSpacing: '2px',
+      msLetterSpacing: '2px',
+      letterSpacing: '2px',
+      fontWeight: '400',
+      fontSize: '22px',
+      color: 'white',
+      borderRadius: '18px',
+      padding: '8px',
+      paddingLeft: '15px',
+      paddingRight: '15px',
+      webkitAppearance: 'none',
+      margin: '1rem 0',
+      textAlign: 'right',
+      margin: '1rem 20px',
+      width: 'auto',
+    },
+    editMentee: {
+      border: 'none',
+      backgroundColor: '#ff914d',
+      fontFamily: 'Caveat Brush,cursive',
+      fontStyle: 'italic',
+      webkitLetterSpacing: '2px',
+      mozLetterSpacing: '2px',
+      msLetterSpacing: '2px',
+      letterSpacing: '2px',
+      fontWeight: '400',
+      fontSize: '22px',
+      color: 'white',
+      borderRadius: '18px',
+      padding: '8px',
+      paddingLeft: '15px',
+      paddingRight: '15px',
+      webkitAppearance: 'none',
+      margin: '1rem 0',
+      textAlign: 'right',
+      margin: '1rem 0',
+      width: 'auto',
+    },
+  };
+
+  const moreInfoHandler = (e, menteeData) => {
+    if (showModal) {
+      // Closing Modal
+      setShowModal(false);
+      setCurrentMentee({});
+      console.log('this got fired!!!', menteeData);
+    } else {
+      // Opening Modal
+      setShowModal(true);
+      setCurrentMentee(menteeData);
+    }
+  };
+  useEffect(() => {
+    props.fetchMentees();
+  }, []);
+
+  return (
+    <div>
+      <ComponentTitles titleText="Student Onboarding" />
+      <Input.Search
+        data-testid="search-bar"
+        addonBefore={<p3>YYYY-MM-DD</p3>}
+        value={search}
+        placeholder={searchBy}
+        style={{ width: '80%', alignSelf: 'center' }}
+        onChange={searchHandler}
+      />
+      <List
+        itemLayout="horizontal"
+        dataSource={menteesSelection}
+        renderItem={item => (
+          <List.Item>
+            <div className="listItemWrapper">
+              <div className="listItemMeta">
+                <List.Item.Meta
+                  avatar={<Avatar src={item.mentee_picture} />}
+                  title={
+                    <a
+                      onClick={e => {
+                        moreInfoHandler(e, item);
+                      }}
+                    >
+                      {item.first_name + ' ' + item.last_name}
+                    </a>
+                  }
+                  description={item.academic_description}
+                />
+              </div>
+              <div className="listItemButtonWrapper">
+                <button
+                  onClick={e => {
+                    moreInfoHandler(e, item);
+                  }}
+                  style={menteeStyles.moreInfo}
+                  className="l2-btn btn "
+                ></button>
+              </div>
+            </div>
+          </List.Item>
+        )}
+      />
+      <Modal
+        className="menteeModal"
+        visible={showModal}
+        title="Mentee Profile"
+        onCancel={moreInfoHandler}
+        maskClosable
+        destroyOnClose
+      >
+        <MenteeProfile currentMentee={currentMentee} />
+      </Modal>
+    </div>
+  );
+};
+const mapStateToProps = state => {
+  return {
+    mentees: state.headmasterReducer.mentees,
+    userId: state.authReducer.userId,
+    role: state.authReducer.role,
+  };
 };
 
-export default studentOnboarding;
+export default connect(mapStateToProps, {
+  checkToken,
+  fetchMentees,
+  editMentee,
+})(StudentOnboarding);
